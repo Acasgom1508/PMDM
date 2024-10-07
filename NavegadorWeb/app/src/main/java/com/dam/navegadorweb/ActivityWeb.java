105a46b4a8b73014743a0cd09617b65a5c8fb24d
Acasgom1508 <166312439+Acasgom1508@users.noreply.github.com>
2024-10-07 10:31:32 +0200

    hago un navegador web

--- NavegadorWeb/app/src/main/java/com/dam/navegadorweb/ActivityWeb.java
+++ NavegadorWeb/app/src/main/java/com/dam/navegadorweb/ActivityWeb.java
@@ -3,7 +3,8 @@ package com.dam.navegadorweb;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,29 +12,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class ActivityWeb extends AppCompatActivity {
 
-    private EditText et1;
+    WebView wv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_web);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        et1 = findViewById(R.id.txt_web);
+        wv1 = findViewById(R.id.web1);
+        String url = getIntent().getStringExtra("sitioWeb");
+        wv1.setWebViewClient(new WebViewClient());
+        wv1.loadUrl("https:\\www." + url);
 
     }
 
-    public void navegar(View view) {
-        Intent intent = new Intent(this, ActivityWeb.class);
-        intent.putExtra("sitioWeb", et1.getText().toString());
+    public void cerrar(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
 
 }
