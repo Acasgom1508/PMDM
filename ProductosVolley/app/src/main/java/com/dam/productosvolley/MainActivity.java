d86baf4509b2af2e1e450b40d0c6a08890254080
Acasgom1508 <166312439+Acasgom1508@users.noreply.github.com>
2024-11-04 10:29:12 +0100

    hago cosas

--- ProductosVolley/app/src/main/java/com/dam/productosvolley/MainActivity.java
+++ ProductosVolley/app/src/main/java/com/dam/productosvolley/MainActivity.java
@@ -1,6 +1,7 @@
 package com.dam.productosvolley;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +9,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 public class MainActivity extends AppCompatActivity {
+
+    private EditText codigo_et, producto_et, precio_et, fab_et;
+    RequestQueue requestQueue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        codigo_et = findViewById(R.id.codigo_et);
+        producto_et = findViewById(R.id.producto_et);
+        precio_et = findViewById(R.id.precio_et);
+        fab_et = findViewById(R.id.fab_et);
+
     }
 }
