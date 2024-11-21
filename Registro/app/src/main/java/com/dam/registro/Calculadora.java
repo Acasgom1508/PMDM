01ae2952533ba6d8733f2f09aa4ee8768d162a7f
Acasgom1508 <166312439+Acasgom1508@users.noreply.github.com>
2024-11-21 13:21:09 +0100

    terminado dia 21

--- Registro/app/src/main/java/com/dam/registro/Calculadora.java
+++ Registro/app/src/main/java/com/dam/registro/Calculadora.java
@@ -3,7 +3,6 @@ package com.dam.registro;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,27 +10,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button boton_ingresar;
+public class Calculadora extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_calculadora);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        boton_ingresar = findViewById(R.id.boton_ingresar);
-
     }
 
-    public void irCalculadora(View view) {
-        startActivity(new Intent(this, Calculadora.class));
+    public void volver(View view) {
+        finish();
     }
 
+    public void irScroll(View view) {
+        startActivity(new Intent(this, pruebaScroll.class));
+    }
 }
--- Registro/app/src/main/java/com/dam/registro/pruebaScroll.java
+++ Registro/app/src/main/java/com/dam/registro/pruebaScroll.java
@@ -1,9 +1,7 @@
 package com.dam.registro;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,27 +9,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button boton_ingresar;
+public class pruebaScroll extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_prueba_scroll);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        boton_ingresar = findViewById(R.id.boton_ingresar);
-
     }
 
-    public void irCalculadora(View view) {
-        startActivity(new Intent(this, Calculadora.class));
+    public void irAtras(View view) {
+        finish();
     }
-
 }
