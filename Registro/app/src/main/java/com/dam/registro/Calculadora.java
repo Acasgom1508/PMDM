946401bc98195ac26ea552a9b0f67e5541d47cb5
Acasgom1508 <166312439+Acasgom1508@users.noreply.github.com>
2024-12-01 23:45:33 +0100

    hago la calculadora un poco funcional

--- Registro/app/src/main/java/com/dam/registro/Calculadora.java
+++ Registro/app/src/main/java/com/dam/registro/Calculadora.java
@@ -5,6 +5,8 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +15,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Calculadora extends AppCompatActivity {
+
+    Button numero1, numero2, numero3, numero4, numero5, numero6, numero7, numero8, numero9, numero0, suma, resta, multiplicacion, division, igual;
+    TextView numeros;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +29,104 @@ public class Calculadora extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        numeros = findViewById(R.id.numeros);
+
+        numero1 = findViewById(R.id.numero1);
+        numero2 = findViewById(R.id.numero2);
+        numero3 = findViewById(R.id.numero3);
+        numero4 = findViewById(R.id.numero4);
+        numero5 = findViewById(R.id.numero5);
+        numero6 = findViewById(R.id.numero6);
+        numero7 = findViewById(R.id.numero7);
+        numero8 = findViewById(R.id.numero8);
+        numero9 = findViewById(R.id.numero9);
+        numero0 = findViewById(R.id.numero0);
+        suma = findViewById(R.id.botonSuma);
+        resta = findViewById(R.id.botonRestar);
+        multiplicacion = findViewById(R.id.botonMult);
+        division = findViewById(R.id.botonDiv);
+        igual = findViewById(R.id.botonIgual);
+
+        numero1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numeros.append("1");
+            }
+        });
+
+        numero2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numeros.append("2");
+            }
+        });
+
+        numero3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numeros.append("3");
+            }
+        });
+
+        numero4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numeros.append("4");
+            }
+        });
+
+        numero5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numeros.append("5");
+            }
+        });
+
+        numero6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numeros.append("6");
+            }
+        });
+
+        numero7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numeros.append("7");
+            }
+        });
+
+        numero8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numeros.append("8");
+            }
+        });
+
+        numero9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numeros.append("9");
+            }
+        });
+
+        numero0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numeros.append("0");
+            }
+        });
+
+
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayShowHomeEnabled(true);
             getSupportActionBar().setLogo(R.mipmap.logo_app);
             getSupportActionBar().setDisplayUseLogoEnabled(true);
             getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#388E3C")));
         }
+
+
     }
 
     public void volver(View view) {
